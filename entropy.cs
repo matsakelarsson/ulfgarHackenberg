cc0d239c69f2a27733c4cd028f3f3bcd8510ab0a
root <m.larsson88@gmail.com>
2019-01-22 21:28:00 +0100

    firat commmit

--- entropy.cs
+++ entropy.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
                 Console.WriteLine("Hello, world!");
                 Console.ReadLine();
+                System.out.println("hejehejhe");
         }
     }
 }
